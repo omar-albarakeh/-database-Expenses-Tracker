4ae0972ff6105bb809561e4842c56ade0e4874af
Omar AlBarakeh <omaralbarakeh2@gmail.com>
2024-11-04 22:18:29 +0200

    js variables initialized

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+const transactionForm = document.getElementById('transaction-form');
+const list = document.getElementById('transactions-list');
+const totalBudget = document.getElementById('total-budget');
