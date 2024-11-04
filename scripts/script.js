3b9eb583c97dc11265d80871076b897592a7ad8b
Omar AlBarakeh <omaralbarakeh2@gmail.com>
2024-11-04 22:19:17 +0200

    local stoarage items getted

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
 const transactionForm = document.getElementById('transaction-form');
 const list = document.getElementById('transactions-list');
 const totalBudget = document.getElementById('total-budget');
+
+let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+
+function updateLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+}
