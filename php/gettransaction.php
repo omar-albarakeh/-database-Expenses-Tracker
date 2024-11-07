da90ab00856bd3044179c395de6de473c55aa460
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-08 01:38:03 +0200

    php <> get transaction

---
+++ php/gettransaction.php
@@ -0,0 +1,14 @@
+<?php
+require "../php/db.php";
+
+$user_id = $_GET['user_id'];
+$sql = "SELECT * FROM transactions WHERE user_id = $user_id ORDER BY created_at DESC";
+$result = $conn->query($sql);
+
+$transactions = [];
+while ($row = $result->fetch_assoc()) {
+    $transactions[] = $row;
+}
+
+echo json_encode($transactions);
+?>
